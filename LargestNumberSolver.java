db7be37a1254b74f15de505514286e043426bc24
Reece Kalmar <ralmarkalmar@gmail.com>
2023-09-18 18:43:36 -0600

    d

--- LargestNumberSolver.java
+++ LargestNumberSolver.java
@@ -5,48 +5,45 @@ import java.util.Comparator;
 import java.util.List;
 
 public class LargestNumberSolver {
-	private static Comparator<? super Integer> comparator	= new Comparator<Integer>() {
-		@Override
-		
-	
-	
-	
-	
 	public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp) {
 		@SuppressWarnings("unchecked")
 		T[] temp = (T[]) new Object[arr.length];
-		for(int i = 0; i < arr.length; i++) {
-			int index;
-		
-			for(int j = i + 1; j < arr.length; j++) {
-				if(cmp.compare(arr[i], arr[j]) < 1)
-					index = i;
-				temp[i] = arr[j];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (cmp.compare(arr[i], arr[j]) < 1)
+					temp[i] = arr[j];
 
-					
-			}}
+			}
+		}
 
 	}
+
 	public static BigInteger findLargestNumber(Integer[] arr) {
-		
+
 	}
+
 	public static int findLargestInt(Integer[] arr) throws OutOfRangeException {
-		
+
 	}
-	public static long findLargestLong(Integer[] arr) throws OutOfRangeException{
-		
+
+	public static long findLargestLong(Integer[] arr) throws OutOfRangeException {
+
 	}
+
 	public static BigInteger sum(List<Integer[]> list) {
-		
+
 	}
-	public static Integer[] findKthLargest(List<Integer[]> list, int k) throws IllegalArgumentException{
-		
+
+	public static Integer[] findKthLargest(List<Integer[]> list, int k) throws IllegalArgumentException {
+
 	}
-	public static List<Integer[]> readFile(String filename){
-		
+
+	public static List<Integer[]> readFile(String filename) {
+
 	}
-	protected class customComparator<T> implements Comparator<T>{
-		public int compare(Integer o1, Integer o2) {
+
+	class customComparator<T> implements Comparator<T> {
+		public int compare(T o1, T o2) {
 			StringBuilder bigNumber1 = new StringBuilder();
 			bigNumber1.append(o1);
 			bigNumber1.append(o2);
@@ -55,24 +52,10 @@ public class LargestNumberSolver {
 			StringBuilder bigNumber2 = new StringBuilder();
 			bigNumber2.append(o2);
 			bigNumber2.append(o1);
-			String num2 = bigNumber1.toString();
+			String num2 = bigNumber2.toString();
 			int secondValue = Integer.parseInt(num2);
 			return firstValue - secondValue;
 
-			
 		}
 	};
-	};
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
--- LargestNumberSolverTest.java
+++ LargestNumberSolverTest.java
@@ -1,15 +1,24 @@
 package assign04;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.Comparator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LargestNumberSolverTest {
+	private Integer[] arr;
 
 	@BeforeEach
-	void setUp() throws Exception {
-	}
+	<T> void setUp() throws Exception {
+		arr = new Integer[] { 1, 2, 3, 4 };
+		Comparator<Integer> custom = new Comparator<Integer>() {
+			public int compare(Integer o1, Integer o2) {
+				return o2 - 1;
+			}
+		};
+		insertionSort(arr, custom);
+
+	};
 
 	@Test
 	void test() {
