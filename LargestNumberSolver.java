647226eed942f72206ab959e43b346fcd5ec4799
Reece Kalmar <ralmarkalmar@gmail.com>
2023-09-20 17:44:04 -0600

    dafsdfasdf

--- LargestNumberSolver.java
+++ LargestNumberSolver.java
@@ -12,73 +12,89 @@ public class LargestNumberSolver {
 			for (j = i - 1; j >= 0 && cmp.compare(arr[j], item) > 0; j--) {
 				arr[j + 1] = arr[j];
 
-			} 
+			}
 			arr[j + 1] = item;
 		}
 
 	}
 
-	public static BigInteger findLargestNumber(Integer[] arr) {
-		if (arr == null || arr.length == 0) {
-            return BigInteger.ZERO; // Return 0 for empty array
-        }
+	public static BigInteger findLargestNumber(Integer[] o1) {
+		if (o1 == null || o1.length == 0) {
+			return BigInteger.ZERO; // Return 0 for empty array
+		}
 
-        // Use custom comparator to sort the array in descending order of concatenated values
-        insertionSort(arr, new customComparator<Integer>());
+		// Use custom comparator to sort the array in descending order of concatenated
+		// values
+		insertionSort(o1, new IntegerComparator<Integer>());
 
-        StringBuilder result = new StringBuilder();
-        for (Integer num : arr) {
-            result.append(num);
-        }
+		StringBuilder result = new StringBuilder();
+		for (Integer num : o1) {
+			result.append(num);
+		}
 
-        return new BigInteger(result.toString());
-    }
-
-
-
-	public static int findLargestInt(Integer[] arr) throws Exception {
-		BigInteger largestNumber = findLargestNumber(arr);
-        
-        if (largestNumber.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0) {
-            throw new Exception("Largest number exceeds int range");
-        }
-
-        return largestNumber.intValue();
+		return new BigInteger(result.toString());
 	}
 
-	public static long findLargestLong(Integer[] arr) throws Exception {
+	public static int findLargestInt(Integer[] arr) throws OutOfRangeException {
 		BigInteger largestNumber = findLargestNumber(arr);
-        
-        if (largestNumber.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0) {
-            throw new Exception("Largest number exceeds long range");
-        }
 
-        return largestNumber.longValue();
-    }
+		if (largestNumber.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0) {
+			throw new OutOfRangeException("Largest number exceeds int range");
+		}
+
+		return largestNumber.intValue();
+	}
+
+	public static long findLargestLong(Integer[] arr) throws OutOfRangeException {
+		BigInteger largestNumber = findLargestNumber(arr);
+
+		if (largestNumber.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0) {
+			throw new OutOfRangeException("Largest number exceeds long range");
+		}
+
+		return largestNumber.longValue();
+	}
 
 	public static BigInteger sum(List<Integer[]> list) {
-
+		BigInteger sum = new BigInteger("0");
+		for(Integer[] intArr: list){
+			sum.add(findLargestNumber(intArr));
+		}
+		return sum;
 	}
 
 	public static Integer[] findKthLargest(List<Integer[]> list, int k) throws IllegalArgumentException {
-
+		if(k < 0 || k >= list.size()){
+			throw new IllegalArgumentException("Kth value is too large or small for the given arrays");
+		}
+		Integer[] sortedList = new Integer[list.size()];
+		for(int i = 0; i < list.size(); i++){
+			sortedList[i] = findLargestInt(list.get(i));
+		}
+		
+		insertionSort(sortedList, new KthIntegerComparator());
+		return sortedList[k];
 	}
 
 	public static List<Integer[]> readFile(String filename) {
 
 	}
 
-	protected static class customComparator<T> implements Comparator<T> {
+	protected static class IntegerComparator<T> implements Comparator<T> {
 		public int compare(T o1, T o2) {
 			String str1 = o1.toString() + o2.toString();
-			String str2 = o2.toString() + o1.toString();
-			Integer num1 = Integer.parseInt(str1);
-			Integer num2 = Integer.parseInt(str2);
-			return num1.compareTo(num2);
-
-		
-
-
+				String str2 = o2.toString() + o1.toString();
+				Integer num1 = Integer.parseInt(str1);
+				Integer num2 = Integer.parseInt(str2);
+				if(num2 - num1 == 0)
+					return 0;
+				return num2 - num1;
+		}
+	};
+	protected static class KthIntegerComparator implements Comparator<Integer> {
+		public int compare(Integer o1, Integer o2) {
+			return o2-o1;
 		}
 	};
 }
+
--- LargestNumberSolverTest.java
+++ LargestNumberSolverTest.java
@@ -1,18 +1,21 @@
 package assign04;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigInteger;
 import java.util.Comparator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LargestNumberSolverTest {
 	private Comparator<Integer> custom;
-	private Integer[] arr, sortedArr;
+	private Integer[] arr, sortedArr, findLargestNumberArr;
 
 	@BeforeEach
 	<T> void setUp() throws Exception {
 		arr =  new Integer[] { 1, 2, 3, 11, 49, 91, 9};
 		sortedArr = new Integer[] {9, 91, 49, 3, 2, 11, 1};
+		findLargestNumberArr = new Integer[] {1 , 2 , 3 , 4 , 5};
 
 		custom = new Comparator<Integer>() {
 			public int compare(Integer o1, Integer o2) {
@@ -29,9 +32,27 @@ class LargestNumberSolverTest {
 	};
 
 	@Test
-	void test() {
+	void insertionSortTest() {
 		LargestNumberSolver.insertionSort(arr, custom);
 		assertArrayEquals(sortedArr, arr);
+	}
+
+	@Test
+	void findLargestNumberTest(){
+		BigInteger actual = new BigInteger("54321");
+		assertEquals(actual, LargestNumberSolver.findLargestNumber(findLargestNumberArr));
+	}
+
+	@Test
+	void findLargestInt() throws Exception{
+		int actual = 54321;
+		assertEquals(actual, LargestNumberSolver.findLargestInt(findLargestNumberArr));
+	}
+	
+	@Test
+	void findLargestLong() throws Exception{
+		long actual = 54321;
+		assertEquals(actual, LargestNumberSolver.findLargestLong(findLargestNumberArr));
 	}
 
 }
